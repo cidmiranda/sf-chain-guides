69e531744d0dbba6589048b9f6f4f5fcd81b1fc2
Cid Miranda <cid.miranda@americanas.io>
2024-01-24 11:45:29 -0300

    develop changes

--- app/index.js
+++ app/index.js
@@ -4,7 +4,6 @@ const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
-const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -12,43 +11,32 @@ const app = express();
 const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
-const p2pServer = new P2pServer(bc, tp);
-const miner = new Miner(bc, tp, wallet, p2pServer);
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
-  res.json(bc.chain);
+    res.json(bc.chain);
 });
 
+
 app.post('/mine', (req, res) => {
-  const block = bc.addBlock(req.body.data);
-  console.log(`New block added: ${block.toString()}`);
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
 
-  p2pServer.syncChains();
+    p2pServer.syncChains();
 
-  res.redirect('/blocks');
+    res.redirect('/blocks');
 });
 
 app.get('/transactions', (req, res) => {
-  res.json(tp.transactions);
+    res.json(tp.transactions);
 });
 
 app.post('/transact', (req, res) => {
-  const { recipient, amount } = req.body;
-  const transaction = wallet.createTransaction(recipient, amount, bc, tp);
-  p2pServer.broadcastTransaction(transaction);
-  res.redirect('/transactions');
-});
-
-app.get('/mine-transactions', (req, res) => {
-  const block = miner.mine();
-  console.log(`New block added: ${block.toString()}`);
-  res.redirect('/blocks');
-});
-
-app.get('/public-key', (req, res) => {
-  res.json({ publicKey: wallet.publicKey });
+    const {recipient, amount} = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('/transactions');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
